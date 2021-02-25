fdc18e628e4a3043ca87b57a09bea0c4b0f53024
unknown <primesurgetechnologies@gmail.com>
2021-02-25 13:58:26 +0500

    Sorting added and table improved

--- src/App.js
+++ src/App.js
@@ -1,155 +1,388 @@
-
-
-import React, { Component } from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import Button from '@material-ui/core/Button';
 
-const coinNames = ["DREP/BTC", "NAV/BTC", "SKY/BTC", "DODO/BTC"]
 
-function TableData(props) {
-  console.log("CCCC", props.data)
-  if (props.data) {
-    return (
-      <div>
-        {
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell align="right">Price</TableCell>
-                <TableCell align="right">BBands Average</TableCell>
-                <TableCell align="right">%</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {Object.keys(props.data).map((item, i) => (
-                <TableRow key={i}>
-                  <TableCell component="th" scope="row">
-                    {props.data[item].Coin}
-                  </TableCell>
-                  <TableCell align="right">{parseFloat(props.data[item].CurrentPrice).toFixed(8)}</TableCell>
-                  <TableCell align="right">{parseFloat(props.data[item].BolingerAverage).toFixed(8)}</TableCell>
-                  <TableCell align="right">{parseFloat(props.data[item].Percentage).toFixed(2)}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          // <Button key={i}
-          //   href={props.this.state.user.social[item].link.includes("http") ? props.this.state.user.social[item].link : "https://" + props.this.state.user.social[item].link}
-          //   target="_blank" size="sm"
-          //   className={socialPlatformClassA(props.this.state.user.social[item].platform)}>
-          //   <i className={socialPlatformClassB(props.this.state.user.social[item].platform)}></i><span>{props.this.state.user.social[item].platform}</span>
-          // </Button>
-          // <Button size="sm" className="btn-facebook btn-brand mr-1 mb-1"><i className="fa fa-facebook"></i><span>Facebook</span></Button>
-
-        }
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <p>
-          No Data
-        </p>
-      </div>
-    )
-  }
-
+function createData(Coin, CurrentPrice, BolingerAverage, Percentage) {
+    return { Coin, CurrentPrice, BolingerAverage, Percentage };
 }
 
+let FinalOutput = []
 
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      Client_details: [],
-      isLoading: false,
-      daysPeriod: 'day',
-      CoinPercentage: [],
-      loading: true,
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
     }
-    this.getCompaniesData = this.getCompaniesData.bind(this)
-    this.getDataAsync = this.getDataAsync.bind(this)
-  }
-
-  async getDataAsync() {
-    let request = []
-    let response = []
-    let data = []
-    let coinLatestPrice = []
-
-    request = 'http://localhost:3001/binance'
-    let details = {
-      method: 'GET',
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
     }
-    response = await fetch(request, details)
-    data = await response.json()
-    return data;
-  }
+    return 0;
+}
 
-  getCompaniesData() {
-    let difference = []
-    let array = []
-    let perdifference;
-    let name2;
-    let FinalOutput = []
-    try {
-      this.getDataAsync()
-        .then(data => {
-          // console.log("AAAA", Object.keys(data))
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
 
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
 
-          Object.keys(data).map((coin) => {
-            console.log("coin name ", data[coin])
-            difference = data[coin]["avgPrice"] - data[coin]["bollMiddle"];
+const headCells = [
+    { id: 'Coin', numeric: false, disablePadding: true, label: 'Coin' },
+    { id: 'CurrentPrice', numeric: true, disablePadding: false, label: 'CurrentPrice' },
+    { id: 'BolingerAverage', numeric: true, disablePadding: false, label: 'BolingerAverage' },
+    { id: 'Percentage', numeric: true, disablePadding: false, label: 'Percentage' },
+];
 
-            perdifference = (difference / data[coin]["bollMiddle"]) * 100;
-            console.log(`percentage differnece in ${coin}`, perdifference)
-            FinalOutput.push({ Coin: coin, CurrentPrice: data[coin]["avgPrice"], BolingerAverage: data[coin]["bollMiddle"], Percentage: perdifference })
-            console.log(`BBBB`, FinalOutput)
+function EnhancedTableHead(props) {
+    const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
 
-
-
-          });
-          this.setState({
-            loading: false,
-            CoinPercentage: FinalOutput
-          }, () => { console.log("final output of coin ", this.state.CoinPercentage) })
-          // console.log("avergae price :",element[k].averagePrice,data[k].valueMiddleBand)
-          // difference=data[name2]["avgPrice"]-data[name2]["bollMiddle"];
-          // console.log("avergae price :",difference,data[name2]["avgPrice"],data[name2]["bollMiddle"])
-          // perdifference=(difference/data[k].valueMiddleBand)*100;
-          // console.log(`percentage differnece in ${ coinNames[k]}`,element[k].averagePrice,data[k].valueMiddleBand)
-          // array.push({coin:coinNames[k],avgDifference:perdifference})
-
-          // }
-
-        });
-    }
-    catch (e) {
-      console.log("Data Not Returned: ", e)
-
-    }
-  }
-
-  componentDidMount() {
-    this.getCompaniesData()
-  }
-
-  render() {
     return (
-      <div >
-        <button onClick={this.getCompaniesData}>Get Data</button>
-        <TableData data={this.state.CoinPercentage} />
-      </div>
+        <TableHead>
+            <TableRow>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        indeterminate={numSelected > 0 && numSelected < rowCount}
+                        checked={rowCount > 0 && numSelected === rowCount}
+                        onChange={onSelectAllClick}
+                        inputProps={{ 'aria-label': 'select all desserts' }}
+                    />
+                </TableCell>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'default'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <span className={classes.visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </span>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
     );
-  }
 }
-export default App;
+
+EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles((theme) => ({
+    root: {
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(1),
+    },
+    highlight:
+        theme.palette.type === 'light'
+            ? {
+                color: theme.palette.secondary.main,
+                backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+            }
+            : {
+                color: theme.palette.text.primary,
+                backgroundColor: theme.palette.secondary.dark,
+            },
+    title: {
+        flex: '1 1 100%',
+    },
+}));
+
+const EnhancedTableToolbar = (props) => {
+    const classes = useToolbarStyles();
+    const { numSelected } = props;
+
+    return (
+        <Toolbar
+            className={clsx(classes.root, {
+                [classes.highlight]: numSelected > 0,
+            })}
+        >
+            {numSelected > 0 ? (
+                <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+                    {numSelected} selected
+                </Typography>
+            ) : (
+                    <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+                        Results
+                    </Typography>
+                )}
+
+            {numSelected > 0 ? (
+                <Tooltip title="Delete">
+                    <IconButton aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                </Tooltip>
+            ) : (
+                    <Tooltip title="Filter list">
+                        <IconButton aria-label="filter list">
+                            <FilterListIcon />
+                        </IconButton>
+                    </Tooltip>
+                )}
+        </Toolbar>
+    );
+};
+
+EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '50%',
+    },
+    paper: {
+        width: '100%',
+        marginBottom: theme.spacing(2),
+    },
+    table: {
+        minWidth: 750,
+    },
+    visuallyHidden: {
+        border: 0,
+        clip: 'rect(0 0 0 0)',
+        height: 1,
+        margin: -1,
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        top: 20,
+        width: 1,
+    },
+}));
+
+export default function EnhancedTable() {
+    const classes = useStyles();
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('Percentage');
+    const [selected, setSelected] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [dense, setDense] = React.useState(true);
+    const [rowsPerPage, setRowsPerPage] = React.useState(15);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = FinalOutput.map((n) => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleClick = (event, name) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1),
+            );
+        }
+
+        setSelected(newSelected);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleChangeDense = (event) => {
+        setDense(event.target.checked);
+    };
+
+    const isSelected = (name) => selected.indexOf(name) !== -1;
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, FinalOutput.length - page * rowsPerPage);
+
+    async function getDataAsync (){
+        let request = []
+        let response = []
+        let data = []
+        let coinLatestPrice = []
+
+        request = 'http://localhost:3001/data'
+        let details = {
+            method: 'GET',
+        }
+        response = await fetch(request, details)
+        data = await response.json()
+        return data;
+    }
+
+    function getCompaniesData () {
+        let difference = []
+        let perdifference;
+
+        FinalOutput = []
+        try {
+            getDataAsync()
+                .then(data => {
+                    Object.keys(data).map((coin) => {
+                        difference = data[coin]["avgPrice"] - data[coin]["bollMiddle"];
+                        perdifference = (difference / data[coin]["bollMiddle"]) * 100;
+                        FinalOutput.push({ Coin: coin, CurrentPrice: data[coin]["avgPrice"], BolingerAverage: data[coin]["bollMiddle"], Percentage: perdifference })
+                        // var tempPrice = data[coin]["avgPrice"]
+                        // var tempAverage = data[coin]["bollMiddle"]
+                        // var temp = {coin, tempPrice, tempAverage , perdifference}
+                        // rows.push(temp)
+                    });
+                    handleRequestSort()
+                    console.log(`BBBB`, FinalOutput)
+                });
+        }
+        catch (e) {
+            console.log("Data Not Returned: ", e)
+        }
+    }
+
+    return (
+        <div className={classes.root}>
+            <br/>
+            <Button variant="contained" color="primary" onClick={getCompaniesData}>Refresh Data</Button>
+
+            <Paper className={classes.paper}>
+                <EnhancedTableToolbar numSelected={selected.length} />
+                <TableContainer>
+                    <Table
+                        className={classes.table}
+                        aria-labelledby="tableTitle"
+                        size={dense ? 'small' : 'medium'}
+                        aria-label="enhanced table"
+                    >
+                        <EnhancedTableHead
+                            classes={classes}
+                            numSelected={selected.length}
+                            order={order}
+                            orderBy={orderBy}
+                            onSelectAllClick={handleSelectAllClick}
+                            onRequestSort={handleRequestSort}
+                            rowCount={FinalOutput.length}
+                        />
+                        <TableBody>
+                            {stableSort(FinalOutput, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row, index) => {
+                                    const isItemSelected = isSelected(row.Coin);
+                                    const labelId = `enhanced-table-checkbox-${index}`;
+
+                                    return (
+                                        <TableRow
+                                            hover
+                                            onClick={(event) => handleClick(event, row.Coin)}
+                                            role="checkbox"
+                                            aria-checked={isItemSelected}
+                                            tabIndex={-1}
+                                            key={row.Coin}
+                                            selected={isItemSelected}
+                                        >
+                                            <TableCell padding="checkbox">
+                                                <Checkbox
+                                                    checked={isItemSelected}
+                                                    inputProps={{ 'aria-labelledby': labelId }}
+                                                />
+                                            </TableCell>
+                                            <TableCell component="th" id={labelId} scope="row" padding="none">
+                                                {row.Coin}
+                                            </TableCell>
+                                            <TableCell align="right">{row.CurrentPrice}</TableCell>
+                                            <TableCell align="right">{row.BolingerAverage}</TableCell>
+                                            <TableCell align="right">{parseFloat(row.Percentage).toFixed(2)}</TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 15]}
+                    component="div"
+                    count={FinalOutput.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
+            <Button variant="contained" color="primary" onClick={getCompaniesData}>Refresh Data</Button>
+            <br/>
+            <FormControlLabel
+                control={<Switch checked={dense} onChange={handleChangeDense} />}
+                label="Dense padding"
+            />
+            
+        </div>
+    );
+}
